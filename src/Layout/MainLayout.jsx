982c1d30ed048847e11952db186e0772e2315a15
slam814 <abdussalam6111997@gmail.com>
2025-09-21 19:27:55 +0600

    i learn react router

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -3,13 +3,13 @@ import NavBar from "../component/Navbar/NavBar";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="max-w-screen-xl mx-auto">
             {/* NaveBar here */}
            <div className="">
               <NavBar></NavBar>
            </div>
             {/* Outlet here */}
-              <div className="min-h-[calc(100vh-200px)]">
+              <div className="min-h-[calc(100vh-125px)]">
                 
                 <Outlet></Outlet>
                 
@@ -17,6 +17,14 @@ const MainLayout = () => {
 
 
             {/* Footer here */}
+<footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 mt-1.5">
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+  </aside>
+</footer>
+
+
+
 
 
         </div>
