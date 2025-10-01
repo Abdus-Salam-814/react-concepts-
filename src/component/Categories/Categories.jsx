bf793732c9930fc79b472c125daebbcd4230aa5a
slam814 <abdussalam6111997@gmail.com>
2025-10-01 22:58:11 +0600

    i learn react router

--- src/component/Categories/Categories.jsx
+++ src/component/Categories/Categories.jsx
@@ -11,11 +11,13 @@ const Categories = ({categories}) => {
     return (
         <div className="text-center space-x-4 my-8 " >
 
-            <div role="tablist" class="tabs tabs-lift justify-center gap-6">
+            <div role="tablist" className="tabs tabs-lift justify-center  w-full">
             {
-                categories.map(category => <NavLink to={`/category/${category.category}`} role="tab" class="tab ">{category.category}</NavLink>)
+                categories.map(category => <NavLink to={`/category/${category.category}`} role="tab" className={({ isActive }) => `tab ${isActive ? 'tab-active':'' } text-xl font-bold `}>{category.category}</NavLink>)
 
             }
+
+           
             </div>
 
             
