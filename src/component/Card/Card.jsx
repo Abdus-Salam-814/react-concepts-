c8be19f74066e3464e575bdf030f75673ffb4cce
slam814 <abdussalam6111997@gmail.com>
2025-10-02 21:59:48 +0600

    i learn react router

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,13 +1,35 @@
+const Card = ({ coffee }) => {
+  const { name, image,  category, type, origin, popularity, rating} = coffee;
 
-const Card = ({category}) => {
- 
-    const {name, image, price} = category;
-
-    return (
-        <div>
-            <h1>{name}</h1>
+  return (
+    <div>
+      <div className="card bg-base-200 shadow-sm">
+        <figure>
+          <img src={`${image}`} alt="" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p className="text-xl">
+            Category: {category}
+          </p>
+          <p className="text-xl">
+            Type: {type}
+          </p>
+          <p className="text-xl">
+            Origin: {origin}
+          </p>
+          <p className="text-xl">
+            rating: {rating}
+          </p>
+          <p className="text-xl">
+            popularity: {popularity}
+          </p>
+          <div className="card-actions justify-end">
+           </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/component/Coffee-card/CoffeeCard.jsx
+++ src/component/Coffee-card/CoffeeCard.jsx
@@ -1,8 +1,11 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 import Card from "../Card/Card";
 import { useEffect, useState } from "react";
 
 const CoffeeCard = () => {
+
+  const navegate = useNavigate();
+
   const { category } = useParams();
   const data = useLoaderData();
 
@@ -21,11 +24,19 @@ const CoffeeCard = () => {
   },[category, data])
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 space-x-4 p-12">
       {coffees.map((coffee) => (
-        <Card key={coffee._id} category={coffee}></Card>
+        <Card key={coffee._id} coffee={coffee}></Card>
       ))}
     </div>
+
+    <div className="p-12">
+                 <button onClick={()=> navegate('/coffees') } className="btn btn-warning ">All vew</button>
+
+    </div>
+    
+    </>
   );
 };
 
